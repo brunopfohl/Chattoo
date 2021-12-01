e8f1471b119bd9ca2a568f9b70f2c0fccf2a20df
brunopfohl <bruno.pfohl@seznam.cz>
2021-12-01 21:51:51 +0100

    uspesne jsem rozbil celej projekt smazání přebytečného kodu smazani veskerych stylu prechod na MUI a emotion

---
+++ Chattoo.GraphQL/ClientApp/src/pages/authentication/register.tsx
@@ -0,0 +1,18 @@
+import { useRouter } from 'next/router';
+import React from 'react'
+import authService from '../../components/api-authorization/AuthorizeService';
+import Loading from '../../components/loading/loading.component';
+
+
+const Register: React.FC = () => {
+    const router = useRouter();
+    const { origin } = router.query;
+
+    authService.signIn(origin);
+
+    return (
+        <Loading detail="Přihlašování"/>
+    );
+}
+
+export default Register;
